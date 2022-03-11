b56905795c77840bbd93bf37e60070674423a746
Mohamed Amin <mohamedamin2009ster@gmail.com>
2022-03-11 10:15:56 -0800

    grabbed DOM elements and initialized states for castleCount skylineCount waterfrontCount and slogans.

--- app.js
+++ app.js
@@ -1,6 +1,27 @@
 // import functions and grab DOM elements
+const cityNameInput = document.getElementById('city-name-input');
+const cityNameEl = document.getElementById('city-name');
+const castleDropDown = document.getElementById('castle-dropdown');
+const skylineDropDown = document.getElementById('skyline-dropdown');
+const waterfrontDropDown = document.getElementById('waterfront-dropdown');
+
+//const countMessageEl = document.getElementById('count-message');
+
+const castleImageEl = document.getElementById('castle-img');
+const skylineImageEl = document.getElementById('skyline-img');
+const waterfrontImageEl = document.getElementById('waterfront-img');
+
+const addSloganButton = document.getElementById('add-slogan-button');
+const sloganInputEl = document.getElementById('slogan-input');
 
 // let state
+let castleCount = 0;
+let skylineCount = 0;
+let waterfrontCount = 0;
+
+const slogans = [];
+
+
 
 
 // set event listeners 
